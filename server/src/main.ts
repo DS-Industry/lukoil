12c5317fbc967ab25e712568465051803545f48d
adamsbear34 <es1230200306@gmail.com>
2023-09-01 17:43:11 +0300

    cors conf

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionFilter());
 
   app.enableCors({
-    origin: ['http://app.lukoil.ds-mobile.ru', 'http://localhost:3000'],
+    origin: ['https://app.lukoil.ds-mobile.ru', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // if you need to send cookies or credentials
   });
