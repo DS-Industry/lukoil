a734510a0be3e7c82b768ce861b39d53c0b89b5a
adamsbear34 <es1230200306@gmail.com>
2023-09-01 16:32:26 +0300

    added cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,12 @@ async function bootstrap() {
   const logger = new Logger('app');
   app.useGlobalFilters(new AllExceptionFilter());
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://app.lukoil.ds-mobile.ru',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if you need to send cookies or credentials
+  });
+
   await app.listen(process.env.PORT, () => {
     logger.log(`LuckOil back-end listening on port ${process.env.PORT}`);
   });
